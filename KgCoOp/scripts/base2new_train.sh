898cfa4100710eee17af7809465d626953c9d222
htyao89 <74002548+coldrainyht@users.noreply.github.com>
2022-11-22 00:05:05 +0800

    Update base2new_train.sh

--- KgCoOp/scripts/base2new_train.sh
+++ KgCoOp/scripts/base2new_train.sh
@@ -4,11 +4,11 @@ cd ..
 
 # custom config
 DATA=/data/yht/data/cl/data
-TRAINER=KgCoOp #SimCoOp
-#TRAINER=SimCoOp
-WEIGHT=8.0
+TRAINER=KgCoOp
+WEIGHT=$2
 DATASET=$1
-CFG=rn50_ep100  # config file
+#CFG=rn50_ep100  # config file\
+CFG=vit_b16_ep100_ctxv1
 CTP=end  # class token position (end or middle)
 NCTX=4  # number of context tokens
 SHOTS=16  # number of shots (1, 2, 4, 8, 16)
@@ -16,7 +16,7 @@ CSC=False  # class-specific context (False or True)
 
 for SEED in 1 2 3
 do
-    DIR=output_temp/base2new/train_base/${DATASET}/shots_${SHOTS}_${WEIGHT}/${TRAINER}/${CFG}/seed${SEED}
+    DIR=output/base2new/train_base/${DATASET}/shots_${SHOTS}_${WEIGHT}/${TRAINER}/${CFG}/seed${SEED}
     if [ -d "$DIR" ]; then
         echo "Results are available in ${DIR}. Skip this job"
     else
